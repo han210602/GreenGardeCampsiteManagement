a1a27f4b204fa1f42d29c28f62c6d6b343660816
Nguyễn Thanh Hậu <101204024+Stella-owo-er@users.noreply.github.com>
2024-09-19 16:35:48 +0700

    setup admin
    some feature of admin dashboard, theme

---
+++ admin/src/theme.js
@@ -0,0 +1,15 @@
+/* type to setup to get all the colors and 
+the types of the typography(kiểu chữ) of the website and 
+allow this to be light and dark mode */
+
+import { createContext, useState, useMemo } from "react";
+import {createTheme} from "@mui/material/styles";
+
+// color design tokens
+#666666
+#141b2d
+#4cceac 
+#db4f4a
+#6870fa
+
+
