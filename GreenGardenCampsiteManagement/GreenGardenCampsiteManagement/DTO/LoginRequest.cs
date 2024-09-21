e832314f1cc6944c9bf53c3b431efcb73d9cef2e
PhungCongTu <tupc2002@gmail.com>
2024-09-21 21:59:47 +0700

    apiLogin

---
+++ GreenGardenCampsiteManagement/GreenGardenCampsiteManagement/DTO/LoginRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace GreenGardenCampsiteManagementAPI.DTO
+{
+    public class User
+    {
+        public int Userid { get; set; } 
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; } // Ví dụ: Admin, User, etc.
+    }
+
+    public class LoginRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+}
--- GreenGardenCampsiteManagement/GreenGardenCampsiteManagement/Program.cs
+++ GreenGardenCampsiteManagement/GreenGardenCampsiteManagement/Program.cs
@@ -1,8 +1,34 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// Thêm JWT Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],  // Sửa lỗi này
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+        RoleClaimType = ClaimTypes.Role 
+    };
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,6 +44,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Thêm Middleware cho Authentication và Authorization
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
