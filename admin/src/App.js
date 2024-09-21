4a948a7d6ec25a7aed51cf12e91a072424e9de02
Nguyễn Thanh Hậu <101204024+Stella-owo-er@users.noreply.github.com>
2024-09-21 15:13:35 +0700

    update new template
    sample template for admin dashboard and management

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,51 +1,106 @@
-import Topbar from "./scenes/global/Topbar";
-import Sidebar from "./scenes/global/Sidebar";
-import Dashboard from "./scenes/dashboard";
-// import Employees from "./scenes/employees";
-// import Invoices from "./scenes/invoices";
-// import Contacts from "./scenes/contacts";
-// import Bar from "./scenes/bar";
-// import Form from "./scenes/form";
-// import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
-// import Geography from "./scenes/geography";
-// import Calendar from "./scenes/calendar";
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-import { ColorModeContext, useMode } from "./theme";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { Routes, Route } from "react-router-dom";
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+// import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 
-function App() {
-  const [theme, colorMode] = useMode();
+import { Ecommerce, Orders, Calendar, Employees, Customers, Kanban, Line, Bar, Pie, ColorPicker, Editor } from './pages';
+
+import './App.css';
+
+import { useStateContext } from './contexts/ContextProvider';
+
+const App = () => {
+  const { setCurrentColor, setCurrentMode, currentMode, activeMenu, currentColor, themeSettings, setThemeSettings } = useStateContext();
+
+  useEffect(() => {
+    const currentThemeColor = localStorage.getItem('colorMode');
+    const currentThemeMode = localStorage.getItem('themeMode');
+    if (currentThemeColor && currentThemeMode) {
+      setCurrentColor(currentThemeColor);
+      setCurrentMode(currentThemeMode);
+    }
+  }, []);
 
   return (
-  <ColorModeContext.Provider value={colorMode}>
-    <ThemeProvider theme={theme}>
-      <CssBaseline/>
-      <div className="app">
-        <main className="content">
-          <Topbar/>
-          <Routes>
-          <Route path="/" element={<Dashboard/>} />
-          {/* <Route path="/employees" element={<Employees />} /> */}
-          {/* <Route path="/contacts" element={<Contacts/>} /> */}
-          {/* <Route path="/invoices" element={<Invoices/>} /> */}
-          {/* <Route path="/form" element={<Form/>} /> */}
-          {/* <Route path="/bar" element={<Bar/>} /> */}
-          {/* <Route path="/pie" element={<Pie/>} /> */}
-          {/* <Route path="/line" element={<Line/>} /> */}
-          {/* <Route path="/faq" element={<FAQ/>} /> */}
-          {/* <Route path="/geography" element={<Geography/>} /> */}
-          {/* <Route path="/calendar" element={<Calendar/>} /> */}
-          </Routes>
-        </main>
-      </div>
+    <div className={currentMode === 'Dark' ? 'dark' : ''}>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
+            <TooltipComponent
+              content="Settings"
+              position="Top"
+            >
+              <button
+                type="button"
+                onClick={() => setThemeSettings(true)}
+                style={{ background: currentColor, borderRadius: '50%' }}
+                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+              >
+                <FiSettings />
+              </button>
 
-    </ThemeProvider>
-  </ColorModeContext.Provider>
-      
+            </TooltipComponent>
+          </div>
+          {/* SIDEBAR */}
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+              <Sidebar />
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              <Sidebar />
+            </div>
+          )}
+          <div
+            className={
+              activeMenu
+                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
+                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
+            }
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+              <Navbar />
+            </div>
+            <div>
+              {themeSettings && (<ThemeSettings />)}
+
+              <Routes>
+                {/* dashboard  */}
+                <Route path="/" element={(<Ecommerce />)} />
+                <Route path="/ecommerce" element={(<Ecommerce />)} />
+
+                {/* pages  */}
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/employees" element={<Employees />} />
+                <Route path="/customers" element={<Customers />} />
+
+                {/* apps  */}
+                <Route path="/kanban" element={<Kanban />} />
+                <Route path="/editor" element={<Editor />} />
+                <Route path="/calendar" element={<Calendar />} />
+                <Route path="/color-picker" element={<ColorPicker />} />
+
+                {/* charts  */}
+                <Route path="/line" element={<Line />} />
+                {/* <Route path="/area" element={<Area />} /> */}
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/pie" element={<Pie />} />
+                {/* <Route path="/financial" element={<Financial />} /> */}
+                {/* <Route path="/color-mapping" element={<ColorMapping />} /> */}
+                {/* <Route path="/pyramid" element={<Pyramid />} /> */}
+                {/* <Route path="/stacked" element={<Stacked />} /> */}
+
+              </Routes>
+            </div>
+            <Footer />
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
   );
-}
+};
 
 export default App;
--- admin/src/index.js
+++ admin/src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import { ContextProvider } from './contexts/ContextProvider';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-  <BrowserRouter>
-  <App />
-  </BrowserRouter>
-  </React.StrictMode>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
-
-
