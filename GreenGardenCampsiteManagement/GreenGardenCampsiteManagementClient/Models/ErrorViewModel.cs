81d991683baa08f528afa3c8e054c7a78b901d20
PhungCongTu <tupc2002@gmail.com>
2024-09-21 22:29:42 +0700

    apiLogin(2)

--- GreenGardenCampsiteManagement/GreenGardenCampsiteManagementClient/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GreenGardenCampsiteManagementClient.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
