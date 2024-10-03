5b284bf8c47fc7f225f7be0dc6ed203206a535bd
Dung020061 <123942736+Dung020061@users.noreply.github.com>
2024-10-03 16:09:33 +0700

    homepage

---
+++ GreenGardenCampsiteClient/GreenGardenCampsiteClient/Controllers/CommonController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GreenGardenCampsiteClient.Controllers
+{
+    public class CommonController : Controller
+    {
+        public IActionResult Service()
+        {
+            return View();
+        }
+
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
