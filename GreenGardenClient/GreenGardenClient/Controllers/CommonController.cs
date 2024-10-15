c860c78bd6603cc891b20de703d3dbd4ddaa3dfd
Skuukzky30 <hoangphihung.hn2002@gmail.com>
2024-10-16 00:48:18 +0700

    frontend

---
+++ GreenGardenClient/GreenGardenClient/Controllers/CommonController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GreenGardenClient.Controllers
+{
+    public class CommonController : Controller
+    {
+        private readonly ILogger<CommonController> _logger;
+
+        public CommonController(ILogger<CommonController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+    }
+}
---
+++ GreenGardenClient/GreenGardenClient/Controllers/ServiceController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GreenGardenClient.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private readonly ILogger<ServiceController> _logger;
+
+        public ServiceController(ILogger<ServiceController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult OrderGear()
+        {
+            return View();
+        }
+        public IActionResult OrderFoodAndDrink()
+        {
+            return View();
+        }
+        public IActionResult FoodDetail()
+        {
+            return View();
+        }
+    }
+}
