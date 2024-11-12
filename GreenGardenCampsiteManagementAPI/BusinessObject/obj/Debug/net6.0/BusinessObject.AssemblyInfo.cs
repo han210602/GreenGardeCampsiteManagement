990678076cdae7b672a6d5fc3c02da7152a01fa4
PhungCongTu <tupc2002@gmail.com>
2024-11-13 00:46:56 +0700

    update

--- GreenGardenCampsiteManagementAPI/BusinessObject/obj/Debug/net6.0/BusinessObject.AssemblyInfo.cs
+++ GreenGardenCampsiteManagementAPI/BusinessObject/obj/Debug/net6.0/BusinessObject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessObject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2a78a8b8c023a10c542656cf8747e0b17c9601a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessObject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessObject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- GreenGardenCampsiteManagementAPI/Repositories/obj/Debug/net6.0/Repositories.AssemblyInfo.cs
+++ GreenGardenCampsiteManagementAPI/Repositories/obj/Debug/net6.0/Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2a78a8b8c023a10c542656cf8747e0b17c9601a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
